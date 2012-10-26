612c4393257a2b0f6bb5b9e9adca8f3e3f8e38ee
Wim Meeussen <wim@wim.gateway.2wire.net>
2012-10-25 17:28:30 -0700

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jenkins_tools',
-    version='0.0.15',
+    version='0.0.16',
     packages=['jenkins_tools'],
     package_dir = {'jenkins_tools':'src/jenkins_tools'},
     scripts = ['scripts/run_chroot_jenkins_now',
