2e42eaf1d92bb628653c636323b4314f863330dd
Wim Meeussen <wim@wim.gateway.2wire.net>
2012-11-08 15:58:34 -0800

    typo

--- src/jenkins_tools/run_jenkins.py
+++ src/jenkins_tools/run_jenkins.py
@@ -15,7 +15,7 @@ JENKINS_SERVER = 'http://jenkins.willowgarage.com:8080/'
 
 
 # Schedule a set of jobs in Jenkins
-def run_jenkins_now(jenkins_instance, ubuntu_distro, arch, name, email, script, script_args, user_name):
+def run_jenkins_now(jenkins_instance, ubuntu_distro, arch, name, email, script, script_args, user_name, parameters=None):
 
     job_xml = pkg_resources.resource_string('jenkins_tools', 'resources/templates/jenkins_template.xml')
     jenkins_conf = pkg_resources.resource_string('jenkins_tools', 'resources/templates/jenkins_conf.yaml')
@@ -35,21 +35,32 @@ def run_jenkins_now(jenkins_instance, ubuntu_distro, arch, name, email, script, 
     params['TIME'] = str(datetime.datetime.now())
     params['USERNAME'] = user_name
     params['HOSTNAME'] = os.uname()[1]
+    params['PARAMETERS'] = ''
+    if parameters and len(parameters) > 0:
+        params['PARAMETERS'] = jc['params'].replace('@(PARAMS)', ' '.join([jc['add_param'].replace('@(PARAM)', p) for p in parameters[0].keys()]))
 
     # replace @(xxx) in template file
     for key, value in params.iteritems():
         job_xml = job_xml.replace("@(%s)"%key, value)
 
     # schedule a new job
-    job_name = "%s-%s-%s-%s"%(params['SCRIPT'], name, params['UBUNTU_DISTRO'], params['ARCH'])
+    job_name = "%s-%s"%(params['SCRIPT'], name)
     if jenkins_instance.job_exists(job_name):
         jenkins_instance.reconfig_job(job_name, job_xml)
         print "Reconfigured job %s"%job_name
     else:
         jenkins_instance.create_job(job_name, job_xml)
         print "Created job %s"%job_name
-    jenkins_instance.build_job(job_name)
-    print "Started job %s"%job_name
+
+    # build all jobs
+    if not param_values:
+        jenkins_instance.build_job(job_name)
+        print "Started job %s"%job_name
+    else:
+        for p in param_values:
+            jenkins_instance.build_job(job_name, p)
+            print "Started job %s with parameters %s"%(job_name, str(p))
+
     print "When the job finishes, you will receive an email at %s"%params['EMAIL']
     return job_name
 
@@ -58,7 +69,7 @@ def run_jenkins_now(jenkins_instance, ubuntu_distro, arch, name, email, script, 
 
 
 # Schedule a set of jobs in Jenkins
-def run_jenkins_periodic(jenkins_instance, ubuntu_distro, arch, name, email, 
+def run_jenkins_periodic(jenkins_instance, ubuntu_distro, arch, name, email,
                          period, script, script_args, user_name):
 
     job_xml = pkg_resources.resource_string('jenkins_tools', 'resources/templates/jenkins_template.xml')
@@ -79,6 +90,7 @@ def run_jenkins_periodic(jenkins_instance, ubuntu_distro, arch, name, email,
     params['TIME'] = str(datetime.datetime.now())
     params['USERNAME'] = user_name
     params['HOSTNAME'] = os.uname()[1]
+    params['PARAMETERS'] = ''
 
     # replace @(xxx) in template file
     for key, value in params.iteritems():
@@ -98,7 +110,7 @@ def run_jenkins_periodic(jenkins_instance, ubuntu_distro, arch, name, email,
 
 
 # Schedule a set of jobs in Jenkins
-def run_jenkins_vcs(jenkins_instance, 
+def run_jenkins_vcs(jenkins_instance,
                     ubuntu_distro, arch, name, email, vcs, uri, branch,
                     script, script_args, user_name):
 
@@ -120,6 +132,7 @@ def run_jenkins_vcs(jenkins_instance,
     params['SCRIPT_ARGS'] = ' '.join(script_args)
     params['USERNAME'] = user_name
     params['HOSTNAME'] = os.uname()[1]
+    params['PARAMETERS'] = ''
 
     # replace @(xxx) in template file
     for key, value in params.iteritems():
