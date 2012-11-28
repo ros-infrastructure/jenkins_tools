c41165067399f39241b94e4c6b8c8e2d5066a205
Wim Meeussen <wim@wim.gateway.2wire.net>
2012-11-28 08:20:16 -0800

    install python-rosdistro by default in chroot

--- scripts/chroot_update.sh
+++ scripts/chroot_update.sh
@@ -6,7 +6,8 @@ apt-get update
 apt-get install -y \
     python-setuptools ccache wget curl curl-ssl sudo git-buildpackage dput python-yaml python-pip python-support \
     git-core mercurial subversion python-all gccxml python-empy python-nose python-mock python-minimock lsb-release \
-    python-numpy python-wxgtk2.8 python-argparse python-networkx graphviz python-sphinx doxygen python-epydoc cmake pkg-config
+    python-numpy python-wxgtk2.8 python-argparse python-networkx graphviz python-sphinx doxygen python-epydoc cmake pkg-config \
+    python-rosdistro
 
 mkdir -p $HOME
 /bin/echo "^^^^^^^^^^^^^^^^^^  update_chroot.sh ^^^^^^^^^^^^^^^^^^^^"
