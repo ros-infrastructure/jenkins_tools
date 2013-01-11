cde4eb3da68d81125669864edf0d9cdfd85b46d8
Julian Cerruti <jcerruti@willowgarage.com>
2013-01-11 18:09:03 -0300

    Add ssh as dependency to be able to build source packages from private github repositories via ssh access

--- scripts/chroot_update.sh
+++ scripts/chroot_update.sh
@@ -6,7 +6,7 @@ apt-get update
 apt-get install -y \
     python-setuptools ccache wget curl curl-ssl sudo git-buildpackage dput python-yaml python-pip python-support \
     git-core mercurial subversion python-all gccxml python-empy python-nose python-mock python-minimock lsb-release \
-    python-numpy python-wxgtk2.8 python-argparse python-networkx graphviz python-sphinx doxygen python-epydoc cmake pkg-config
+    python-numpy python-wxgtk2.8 python-argparse python-networkx graphviz python-sphinx doxygen python-epydoc cmake pkg-config openssh-client
 
 
 mkdir -p $HOME
