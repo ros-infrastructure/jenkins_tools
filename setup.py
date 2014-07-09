c022d8be9f32423b53e5ed40d57c4eb1aa6456d6
Tully Foote <tfoote@osrfoundation.org>
2014-07-08 17:27:32 -0700

    0.0.68

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='jenkins_tools',
-    version='0.0.67',
+    version='0.0.68',
     packages=['jenkins_tools'],
     package_dir={'jenkins_tools': 'src/jenkins_tools'},
     scripts=['scripts/run_chroot_jenkins_now',
